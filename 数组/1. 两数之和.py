5b7baafe03b3f6bf0ab4043111d316240b11c668
ahcdw <xing.wang@qiaofang.com>
2021-04-25 22:04:42 +0800

    leetcode python

---
+++ 数组/1. 两数之和.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(0, len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+
+    index = twoSum(None, [3, 2, 4], 6)
+    print(index)
